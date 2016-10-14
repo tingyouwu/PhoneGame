9b123955f981c4ecbe6065eb6444af0c00119c07
tingyouwu <wutingyou@wxchina.com>
2016-10-14 16:54:44 +0800

    提交代码

--- PhoneGame/app/src/main/java/com/wty/app/phonegame/LoginActivity.java
+++ PhoneGame/app/src/main/java/com/wty/app/phonegame/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.wty.app.phonegame;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btn_start;
     Button btn_end;
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    private BluetoothAdapter mBluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btn_start = (Button) findViewById(R.id.btn_startgame);
         btn_start.setOnClickListener(this);
         btn_end.setOnClickListener(this);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "手机无蓝牙设备", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }else{
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
+        }
+
     }
 
 
@@ -29,6 +57,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }else if(v.getId()==R.id.btn_startgame){
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                } else {
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
         }
     }
 }
